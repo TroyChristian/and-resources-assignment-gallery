18cd072d1cca269556937a708827f285307a6bcc
Troy Christian <apostasy8@gmail.com>
2019-08-12 13:23:07 -0700

    @VivekV95

---
+++ AndResourcesTroy/app/src/main/java/com/example/andresourcestroy/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.andresourcestroy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+//comment
+//comment to commit and add Vivek
